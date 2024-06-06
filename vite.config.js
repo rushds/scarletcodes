c7f55019e84b1277585382661d1133b553c35534
Rushd Syed <rushd.syed@gmail.com>
2024-06-06 15:34:04 -0400

    pls what

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,3 @@ export default defineConfig({
   plugins: [react()],
 });
 
-import AWS from 'aws-sdk';
-import awsmobile from './aws-exports';
-
-/*
- * Configure the SDK to use anonymous identity 
- */
-AWS.config.update({
-  region: awsmobile.aws_cognito_region,
-  credentials: new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: awsmobile.aws_cognito_identity_pool_id
-  })
-});
-
